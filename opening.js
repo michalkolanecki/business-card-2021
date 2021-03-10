f909d7dfd325df0f62925bb6348a4feae495c1dd
Michał Kolanecki <kolanek2@wp.pl>
2021-03-10 07:21:00 +0100

    last style changes

---
+++ opening.js
@@ -0,0 +1,10 @@
+const age = 35;
+const name = "Michał";
+
+function welcome(age, name) {
+    console.log(
+        `Witam serdecznie na przykładowej stronie, nazywam się ${name} i mam ${age} lat.`
+    );  
+}
+
+welcome(age, name);
